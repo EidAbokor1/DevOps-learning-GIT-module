e41ccf54adfd924755497acecad4b77c4597bf9c
EidAbokor1 <abokoreid1@gmail.com>
2025-07-12 17:07:07 +0100

    add s3 bucker terraformtf

---
+++ main.tf
@@ -0,0 +1,14 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-terraform-bucket-example"
+  acl    = "private"
+
+  tags = {
+    Name        = "MyS3Bucket"
+    Environment = "Dev"
+  }
+}
